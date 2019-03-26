8719d2f4e31dfecd995044e6c3017149a5f0a5ba
Bernhard Schäfer <bernhard.schaefer@sap.com>
2019-03-26 17:17:13 +0100

    pytorch and regression eval utils

---
+++ yamlu/pytorch.py
@@ -0,0 +1,66 @@
+import warnings
+
+import sklearn.metrics
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.utils.data import DataLoader
+
+
+def score(model: nn.Module, dl: DataLoader, with_loss=True):
+    model.eval()
+
+    with torch.no_grad():
+        y_scores, y_true = list(zip(*((model(xb), yb) for xb, yb in dl)))
+        y_scores, y_true = torch.cat(y_scores), torch.cat(y_true)
+        y_pred = torch.argmax(y_scores, dim=1, keepdim=False)
+
+        if with_loss:
+            losses = F.cross_entropy(y_scores, y_true, reduction='none')
+            return y_true, y_pred, losses
+        else:
+            return y_true, y_pred
+
+
+def evaluate(model: nn.Module, valid_dl: DataLoader, split: str):
+    model.eval()
+
+    with torch.no_grad():
+        y_scores, y_true = [], []
+        for xb, yb in valid_dl:
+            y_true.append(yb)
+            y_scores.append(model(xb))
+        return calc_metrics(y_true, y_scores, split)
+
+
+def calc_metrics(y_true, y_scores, split: str):
+    with torch.no_grad():
+        # concatenate tensors in case argument is a list of batches
+        if isinstance(y_true, list):
+            y_true = torch.cat(y_true)
+        if isinstance(y_scores, list):
+            y_scores = torch.cat(y_scores)
+
+        n_classes = 1 if y_scores.dim == 1 else y_scores.shape[1]
+
+        loss = F.cross_entropy(y_scores, y_true).item()
+
+        y_true = y_true.numpy()
+        if n_classes > 1:
+            y_pred = torch.argmax(y_scores, dim=1).numpy()
+        else:
+            y_pred = (y_scores > .5).numpy()
+
+    acc = sklearn.metrics.accuracy_score(y_true, y_pred)
+
+    average = "binary" if n_classes == 1 else "macro"
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        p = sklearn.metrics.precision_score(y_true, y_pred, average=average)
+        r = sklearn.metrics.recall_score(y_true, y_pred, average=average)
+
+    m = {f"{split}_loss": loss,
+         f"{split}_acc": acc,
+         f"{split}_prec": p,
+         f"{split}_rec": r}
+    return m
---
+++ yamlu/regression.py
@@ -0,0 +1,45 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def regression_metrics_npy(y_test, y_pred, col_names):
+    y_test_df = pd.DataFrame(y_test, columns=col_names)
+    y_pred_df = pd.DataFrame(y_pred, columns=col_names)
+    return regression_metrics(y_test_df, y_pred_df)
+
+
+def regression_metrics(y_test, y_pred, name=""):
+    if y_test.ndim > 1 and y_test.shape[1] > 1:  # multiple output scenario
+        # recursively call function for each column and combine results
+        return pd.concat(
+            regression_metrics(y_test[col], y_pred[col], name="_".join([col, name])) for col in y_test.columns)
+
+    # extract values if necessary
+    if type(y_test) == pd.DataFrame and y_test.shape[1] == 1:
+        y_test = y_test.iloc[:, 0]
+    if type(y_pred) == pd.DataFrame and y_pred.shape[1] == 1:
+        y_pred = y_pred.iloc[:, 0]
+    if type(y_test) == pd.Series:
+        y_test = y_test.values
+    if type(y_pred) == pd.Series:
+        y_pred = y_pred.values
+
+    res_df = pd.DataFrame({
+        'target': name,
+        'y_test': y_test,
+        'y_pred': y_pred,
+        'ae': np.abs(y_test - y_pred),
+        'se': (y_test - y_pred) ** 2
+    })
+    res_df['ape'] = (res_df['ae'] / res_df.y_test) * 100.
+    mse = np.mean(res_df.se)
+    mae = np.mean(res_df.ae)
+    mape = np.mean(res_df.ape)
+    median_ae = np.median(res_df.se)
+    logger.info(
+        f"{name} metric: mse={mse:.3f}, mae={mae:.3f}, mape={mape:.3f}%, median_ae={median_ae:.3f}")
+    return pd.Series(dict(mse=mse, mae=mae, mape=mape), name=name).to_frame().T

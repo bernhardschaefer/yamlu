45dfea0ceb77300575d1ce9586e394a08511f546
Bernhard Schäfer <bernhard.schaefer@sap.com>
2020-03-16 09:34:57 +0100

    clean up regression metrics

--- yamlu/regression.py
+++ yamlu/regression.py
@@ -1,40 +1,69 @@
 import logging
+from typing import Union
 
 import numpy as np
 import pandas as pd
+import torch
 
 logger = logging.getLogger(__name__)
 
 
-def regression_metrics_npy(y_test, y_pred, col_names):
-    y_test_df = pd.DataFrame(y_test, columns=col_names)
-    y_pred_df = pd.DataFrame(y_pred, columns=col_names)
-    return regression_metrics(y_test_df, y_pred_df)
+def regression_metrics(y_true: Union[np.ndarray, torch.Tensor],
+                       y_pred: Union[np.ndarray, torch.Tensor],
+                       target_names: List[str]):
+    """
+    Regression metric calculation that supports both multiple targets and single targets scenario.
+    Result can be visualized e.g. using `metrics[["mse", "mae"]].plot.bar(figsize=(8,4));`
+    :param y_true: true
+    :param y_pred: predictions
+    :param target_names a list of target names
+    :return: a pandas DataFrame with metrics as columns and target names as rows
+    """
+    if isinstance(y_true, torch.Tensor):
+        y_true = y_true.cpu().numpy()
+    if isinstance(y_pred, torch.Tensor):
+        y_pred = y_pred.cpu().numpy()
+    y_true_df = pd.DataFrame(y_true, columns=target_names)
+    y_pred_df = pd.DataFrame(y_pred, columns=target_names)
+    return _regression_metrics(y_true_df, y_pred_df)
 
 
-def regression_metrics(y_test, y_pred, name=""):
-    if y_test.ndim > 1 and y_test.shape[1] > 1:  # multiple output scenario
+def regression_metrics_pandas(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> pd.DataFrame:
+    """
+    Regression metric calculation that supports both multiple targets and single targets scenario.
+    Result can be visualized e.g. using `metrics[["mse", "mae"]].plot.bar(figsize=(8,4));`
+    :param y_true: true
+    :param y_pred: predictions
+    :return: a pandas DataFrame with metrics as columns and target names as rows
+    """
+    return _regression_metrics(y_true, y_pred)
+
+
+def _regression_metrics(y_true: Union[pd.DataFrame, pd.Series, np.ndarray],
+                        y_pred: Union[pd.DataFrame, pd.Series, np.ndarray],
+                        name: str = "") -> pd.DataFrame:
+    if y_true.ndim > 1 and y_true.shape[1] > 1:  # multiple output scenario
         # recursively call function for each column and combine results
-        return pd.concat(regression_metrics(y_test[col], y_pred[col], name=col) for col in y_test.columns)
+        return pd.concat(regression_metrics(y_true[col], y_pred[col], name=col) for col in y_true.columns)
 
     # extract values if necessary
-    if type(y_test) == pd.DataFrame and y_test.shape[1] == 1:
-        y_test = y_test.iloc[:, 0]
-    if type(y_pred) == pd.DataFrame and y_pred.shape[1] == 1:
+    if isinstance(y_true, pd.DataFrame) and y_true.shape[1] == 1:
+        y_true = y_true.iloc[:, 0]
+    if isinstance(y_pred, pd.DataFrame) and y_pred.shape[1] == 1:
         y_pred = y_pred.iloc[:, 0]
-    if type(y_test) == pd.Series:
-        y_test = y_test.values
-    if type(y_pred) == pd.Series:
+    if isinstance(y_true, pd.Series):
+        y_true = y_true.values
+    if isinstance(y_pred, pd.Series):
         y_pred = y_pred.values
 
     res_df = pd.DataFrame({
         'target': name,
-        'y_test': y_test,
+        'y_true': y_true,
         'y_pred': y_pred,
-        'ae': np.abs(y_test - y_pred),
-        'se': (y_test - y_pred) ** 2
+        'ae': np.abs(y_true - y_pred),
+        'se': (y_true - y_pred) ** 2
     })
-    res_df['ape'] = (res_df['ae'] / res_df.y_test) * 100.
+    res_df['ape'] = (res_df['ae'] / res_df.y_true) * 100.
     mse = np.mean(res_df.se)
     mae = np.mean(res_df.ae)
     mape = np.mean(res_df.ape)

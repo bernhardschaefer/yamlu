5f9a95d7f670bd63e36635ab7e079480ea42b534
Bernhard Schäfer <bernhard.schaefer@sap.com>
2020-03-24 13:46:53 +0100

    pickle cache decorator

--- yamlu/misc.py
+++ yamlu/misc.py
@@ -1,12 +1,40 @@
+import functools
 import logging
 import os
+import pickle
 from contextlib import contextmanager
+from pathlib import Path
 from typing import List
+
+_logger = logging.getLogger(__name__)
 
 
 def set_gpu_ids(gpu_ids: List[int]):
     gpu_ids_str = ",".join(map(str, gpu_ids))
     os.environ['CUDA_VISIBLE_DEVICES'] = gpu_ids_str
+
+
+def pickle_cache(cache_path: Path):
+    """
+    Decorator to cache the result of a function in a pickle file.
+    :param cache_path: the path where to cache the file
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def inner():
+            if cache_path.is_file():
+                with cache_path.open("rb") as f:
+                    return pickle.load(f)
+            res = func()
+            _logger.info("Caching object type %s to %s", type(res), cache_path)
+            with cache_path.open("wb") as f:
+                pickle.dump(res, f)
+            return res
+
+        return inner
+
+    return decorator
 
 
 @contextmanager

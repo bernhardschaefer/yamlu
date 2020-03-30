a1e01b83849e06ce2881af39bba97e7461a5e0aa
Bernhard Schäfer <bernhard.schaefer@sap.com>
2020-03-30 09:52:20 +0200

    deterministic seed

--- yamlu/misc.py
+++ yamlu/misc.py
@@ -2,11 +2,38 @@ import functools
 import logging
 import os
 import pickle
+import random
 from contextlib import contextmanager
+from datetime import datetime
 from pathlib import Path
 from typing import List
 
+import numpy as np
+import torch
+
 _logger = logging.getLogger(__name__)
+
+
+# copied from detectron2
+# https://github.com/facebookresearch/detectron2/blob/master/detectron2/utils/env.py
+def seed_all_rng(seed=None):
+    """
+    Set the random seed for the RNG in torch, numpy and python.
+
+    Args:
+        seed (int): if None, will use a strong random seed.
+    """
+    if seed is None:
+        seed = (
+                os.getpid()
+                + int(datetime.now().strftime("%S%f"))
+                + int.from_bytes(os.urandom(2), "big")
+        )
+        logger = logging.getLogger(__name__)
+        logger.info("Using a generated random seed {}".format(seed))
+    np.random.seed(seed)
+    torch.set_rng_state(torch.manual_seed(seed).get_state())
+    random.seed(seed)
 
 
 def set_gpu_ids(gpu_ids: List[int]):

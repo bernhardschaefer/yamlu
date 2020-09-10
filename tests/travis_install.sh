3534c7bbb0511277c641eb052fb4a7d7a165490c
Bernhard Schäfer <bernhard.schaefer@sap.com>
2020-09-10 16:25:49 +0200

    wip travis: remove tox and coverage

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -41,12 +41,6 @@ fi
 
 # for all
 pip install -U pip setuptools
-pip install tox
-
-if [[ "$COVERAGE" == "true" ]]; then
-    pip install -U pytest-cov pytest-virtualenv coverage coveralls flake8 pre-commit
-fi
-
 
 travis-cleanup() {
     printf "Cleaning up environments ... "  # printf avoids new lines

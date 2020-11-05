19a1d9e54d948da36d554df11d25e3befdc4bd5b
Bernhard Schäfer <bernhard.schaefer@sap.com>
2020-11-05 20:36:53 +0100

    exclude __MACOSX and desktop.ini

--- yamlu/path.py
+++ yamlu/path.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import List, Iterator
 
+EXCLUDED_FILE_NAMES = {"__MACOSX", "desktop.ini"}
+
 
 def glob(path: Path, pattern: str, include_hidden=False) -> List[Path]:
     return _get_sorted_paths(path.glob(pattern), include_hidden)
@@ -12,6 +14,8 @@ def ls(path: Path, include_hidden=False) -> List[Path]:
 
 def _get_sorted_paths(path_iterator: Iterator[Path], include_hidden: bool) -> List[Path]:
     def filter_fn(p: Path):
+        if p.name in EXCLUDED_FILE_NAMES:
+            return False
         if include_hidden:
             return True
         if p.name.startswith("."):

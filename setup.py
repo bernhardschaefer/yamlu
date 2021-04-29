c86c26ea69639257d5b25931d868c6a5114b4d52
Bernhard Schäfer <bernhard.schaefer@sap.com>
2021-04-29 15:52:00 +0200

    add install_requires

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="yamlu",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "Pillow"
+    ]
 )

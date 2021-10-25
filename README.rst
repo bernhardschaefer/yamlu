9161b1198cff21b22c5459afc79e4210106a6c8f
Bernhard Schäfer <bernhard.schaefer@sap.com>
2021-10-25 11:43:51 +0200

    migrate to pyscaffold

---
+++ README.rst
@@ -0,0 +1,34 @@
+.. image:: https://github.com/<USER>/yamlu/actions/workflows/run_tests.yaml/badge.svg
+    :alt: Built status 
+    :target: https://github.com/<USER>/yamlu/actions/workflows/run_tests.yaml
+.. image:: https://img.shields.io/coveralls/github/<USER>/yamlu/main.svg
+    :alt: Coveralls
+    :target: https://coveralls.io/r/<USER>/yamlu
+.. image:: https://img.shields.io/pypi/v/yamlu.svg
+    :alt: PyPI-Server
+    :target: https://pypi.org/project/yamlu/
+.. image:: https://img.shields.io/badge/-PyScaffold-005CA0?logo=pyscaffold
+    :alt: Project generated with PyScaffold
+    :target: https://pyscaffold.org/
+
+|
+
+=====
+yamlu - yet another machine learning utility library
+
+=====
+
+
+    Add a short description here!
+
+
+A longer description of your project goes here...
+
+
+.. _pyscaffold-notes:
+
+Note
+====
+
+This project has been set up using PyScaffold 4.1.1. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup, find_packages
+"""
+    Setup file for yamlu.
+    Use setup.cfg to configure your project.
 
-setup(
-    name="yamlu",
-    version="0.0.7",
-    description="yet another machine learning utility library",
-    url="https://github.com/bernhardschaefer/yamlu",
-    author="Bernhard Schäfer",
-    author_email="bernhard.schaefer1+pypi@gmail.com",
-    license="Apache 2.0",
-    packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=[
-        "matplotlib",
-        "numpy",
-        "Pillow"
-    ]
-)
+    This file was generated with PyScaffold 4.1.1.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+from setuptools import setup
+
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
